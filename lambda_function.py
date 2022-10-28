9caff924f28a500d43888f56ec68ef86ed94ff39
rukiaaidid <35967742+rukiaaidid@users.noreply.github.com>
2022-10-28 15:17:45 +0100

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -1,57 +1,35 @@
-import json
-import boto3
-import botocore
 
-dynamodb = boto3.resource('dynamodb')
-table_name = 'data'
-table = dynamodb.Table(table_name)
-client = boto3.client('dynamodb')
 
 def lambda_handler(event, context):
-    
-    try:
-        checkTable(table_name)
-        table.put_item(Item=event)
-        
-        return{'statusCode': 200, 'message': 'Data inserted successfully.'}
-    except botocore.exceptions.ClientError as error:
-        print(error)
-        
-def checkTable(table_name):
-    try:
-        response = client.list_tables()
-        
-        if table_name in response ['TableNames']:
-            table_found = True
-        
-        else:
-            table_found = False
-            
-            response = client.create_table(
-                TableName = table_name,
-                AttributeDefinitions = [
-                    {
-                        'AttributeName': 'id',
-                        'AttributeType': 'S'
-                    },
-                ],
-                KeySchema=[
-                    {
-                        
-                        'AttributeName': 'id',
-                        'KeyType': 'HASH'
-                    },
-                ],
-                ProvisionedThroughput={
-                    'ReadCapacityUnits': 5,
-                    'WriteCapacityUnits': 5
-                }
-            )
-            
-            table.meta.client.get_waiter(
-                'table_exists').wait(TableName=table_name)
-                
-    except botocore.exceptions.ClientError as error:
-        print(error)
-        
-    return table_found
+    message = """
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width,
+        initial-scale=1.0" />
+    <title> Final Project</title>
+    <link rel="stylesheet" href="main.css" />
+</head>
+
+<body>
+    <nav>
+        <div class="logo">EDM</div>
+        <div class="nav-items">
+            <a href="index.html">Home </a>
+            <a href="data.html"> Data </a>
+            <a href="form.html">Survey</a>
+        </div>
+    </nav>
+    <section class="hero">
+        <div class="hero-container">
+            <div class="column-left">
+                <h1>Climate Change</h1>
+                <p>Where we are and what we can do.</p>
+                <button> <a href="data.html"> Get Started </a> </button>
+            </div>
+        </div>
+    </section>
+</body>
+
+</html>
